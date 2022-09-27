da139bc247b47e1a33d2ba84b276d627c21202f1
Boye Borg Nygård <6630430+boyeborg@users.noreply.github.com>
2022-09-27 14:34:14 +0200

    create repro

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import plugin from "./plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    plugin(),
     svelte({
       compilerOptions: {
         hydratable: true
